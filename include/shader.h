23f4b77af4cf0e60a0a6bd0c01e6e946aba40a24
Tony Wang <jiaweitwang@gmail.com>
2016-12-03 14:18:19 -0800

    rip

--- include/shader.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHADER_H
-#define SHADER_H
-
-char* readTextFile(char* aTextFile);
-
-int setShader(char* shaderType, char* shaderFile);
-
-#endif
---
+++ shader.h
@@ -0,0 +1,8 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+char* readTextFile(char* aTextFile);
+
+int setShader(char* shaderType, char* shaderFile);
+
+#endif
