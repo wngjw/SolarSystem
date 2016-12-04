68b2611fdaffffc037536db8dcaadfe64e31f9af
Tony Wang <jiaweitwang@gmail.com>
2016-12-03 20:25:05 -0800

    Revert "for real pls no more sphereinder"
    This reverts commit 9f21466318c4dd492739e97f8eed99fe1fc3b23d.

--- sphere.h
+++ sphere.h
@@ -7,18 +7,14 @@ const float PI = 3.14159265;
 const int SPHERE_LONGS = 50;
 const int SPHERE_LATS = 50;
 
-class Sphere {
-public:
-    void fillSphereVertexArray(Vertex sphereVertices[(SPHERE_LONGS + 1) * (SPHERE_LATS + 1)]);
-    void fillSphereIndices(unsigned int sphereIndices[SPHERE_LATS][2*(SPHERE_LONGS+1)]);
-    void fillSphereCounts(int sphereCounts[SPHERE_LATS]);
-    void fillSphereOffsets(void* sphereOffsets[SPHERE_LATS]);
+void fillSphereVertexArray(Vertex sphereVertices[(SPHERE_LONGS + 1) * (SPHERE_LATS + 1)]);
+void fillSphereIndices(unsigned int sphereIndices[SPHERE_LATS][2*(SPHERE_LONGS+1)]);
+void fillSphereCounts(int sphereCounts[SPHERE_LATS]);
+void fillSphereOffsets(void* sphereOffsets[SPHERE_LATS]);
 
-    void fillSphere(Vertex sphereVertices[(SPHERE_LONGS + 1) * (SPHERE_LATS + 1)],
-                 unsigned int sphereIndices[SPHERE_LATS][2 * (SPHERE_LONGS+1)],
-                 int sphereCounts[SPHERE_LATS],
-                 void* sphereOffsets[SPHERE_LATS]);
-};
-
+void fillSphereinder(Vertex sphereVertices[(SPHERE_LONGS + 1) * (SPHERE_LATS + 1)],
+	         unsigned int sphereIndices[SPHERE_LATS][2 * (SPHERE_LONGS+1)],
+			 int sphereCounts[SPHERE_LATS],
+			 void* sphereOffsets[SPHERE_LATS]);
 
 #endif
