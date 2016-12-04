7abe2a79ff7b20958d865bff59c3f9054a4a8830
Tony Wang <jiaweitwang@gmail.com>
2016-12-03 20:27:10 -0800

    The last laugh

--- sphere.h
+++ sphere.h
@@ -12,7 +12,7 @@ void fillSphereIndices(unsigned int sphereIndices[SPHERE_LATS][2*(SPHERE_LONGS+1
 void fillSphereCounts(int sphereCounts[SPHERE_LATS]);
 void fillSphereOffsets(void* sphereOffsets[SPHERE_LATS]);
 
-void fillSphereinder(Vertex sphereVertices[(SPHERE_LONGS + 1) * (SPHERE_LATS + 1)],
+void fillSphere(Vertex sphereVertices[(SPHERE_LONGS + 1) * (SPHERE_LATS + 1)],
 	         unsigned int sphereIndices[SPHERE_LATS][2 * (SPHERE_LONGS+1)],
 			 int sphereCounts[SPHERE_LATS],
 			 void* sphereOffsets[SPHERE_LATS]);
